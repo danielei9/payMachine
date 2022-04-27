f91a827307546f7181c93b5493ff3de59920c759
Daniel Burruchaga <dbs99@hotmail.com>
2022-04-27 21:26:29 +0200

    init

---
+++ CoinWallet.py
@@ -0,0 +1,13 @@
+import Serial_ 
+
+def coinWallet():
+    #initialize the 
+    def init():
+    def reset():
+    def status():
+    def tubeStatus():
+    def poll():
+    def coinType():
+    def dispense():
+    
+
