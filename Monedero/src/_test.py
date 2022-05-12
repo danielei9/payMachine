e641ef3f2753bd55f3744b74a6c7cb35d00eaff3
danielei9 <dbs99@hotmail.com>
2022-05-12 23:51:14 +0200

    Revert "Revert "Add files via upload""
    This reverts commit bc37c698bd39233f5659acb8da981c4bc9ede247.

--- Monedero/src/_test.py
+++ Monedero/src/_test.py
@@ -1,7 +1,10 @@
 import CoinWallet 
 import time
+def cb(data):
+    print("cb " + str(data))
+    
+cW = CoinWallet.CoinWallet(cb)
 
-cW = CoinWallet.CoinWallet()
 time.sleep(1)
 cW.reset()
 cW.setup()
